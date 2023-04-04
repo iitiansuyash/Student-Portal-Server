8c8db0e0e02262d2c322a7cbc83a3ca034264785
Niket Gupta <niketgupta101@gmail.com>
2023-04-04 19:31:59 +0530

    Entity changed

--- src/entity/Spec_Offered_Acadyear.ts
+++ src/entity/Spec_Offered_Acadyear.ts
@@ -1,4 +1,4 @@
-import { Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Academic_Year } from "./Academic_Year";
 import { Specialization } from "./Specialization";
 
@@ -9,9 +9,11 @@ export class Spec_Offered_Acadyear{
     public idacadyear_spec_rel: number
 
     @ManyToOne(() => Specialization, (spec) => spec.specToAcadyear)
+    @JoinColumn({ name: 'specId'})
     public spec: Specialization
 
     @ManyToOne(() => Academic_Year, (acadYear) => acadYear.acadYearToSpec)
+    @JoinColumn({ name: 'acadYear' })
     public acadYear: Academic_Year
 
 }
